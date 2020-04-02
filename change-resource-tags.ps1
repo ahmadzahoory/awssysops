a58da01b71eb9883f925791ec35e2bbcf5517de3
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2020-04-02 21:12:05 +0530

    Delete change-resource-tags.ps1

--- change-resource-tags.ps1
+++
@@ -1,5 +0,0 @@
-$instances = aws ec2 describe-instances --filter "Name=tag:Project,Values=ERPSystem" "Name=tag:Environment,Values=development" --query 'Reservations[*].Instances[*].InstanceId' --output text
-
-$instances = $instances.split("\n")
-
-aws ec2 create-tags --resources $instances --tags 'Key=Version,Value=1.1'
